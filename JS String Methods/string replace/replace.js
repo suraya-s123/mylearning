18890ec2fbc8ff704527d1f11df036f0048156e8
suraya-s123 <sarminsuraya@gmail.com>
2023-12-13 17:16:21 +0600

    add pad(), trim(), concat(), lowerCase(), UpperCase(), replaceAll()

--- JS String Methods/string replace/replace.js
+++ JS String Methods/string replace/replace.js
@@ -10,3 +10,44 @@ let txt= "I love dogs. Dogs are very easy to love. Dogs are very popular ";
  txt= txt.replaceAll("Dogs","animal");
 
 console.log(txt);
+
+// toUppercase() LowerCase use
+let cases1 ="I am A Student of Southest University";
+console.log(cases1.toUpperCase());
+console.log(cases1.toLowerCase());
+
+// JavaScript String concat()
+ 
+let test1 = "Hello";
+let text2= "world";
+let text3 = test1.concat(" ", text2);
+console.log(text3.concat(" ", text2));
+
+// JavaScript String trim()
+let trim1= "     I am     a    student  ";
+let trim2= trim1.trim();
+console.log(trim2);
+console.log("lenght trim1 = " + trim1.length + "<br>lenth trim2= " + trim2.length );
+let wt4="    right"
+let rmv
+console.log(wt4.trimStart());
+console.log(wt4.length);
+
+
+// padStart()
+ 
+let pad= "59"; // index 01
+let padded= pad.padStart(9,"suraya "); // 01+...9 with whitspace 
+ 
+console.log(pad)
+
+console.log(padded); //output = suraya 59
+
+
+
+let pad1= "59";
+let padded1= pad1.padEnd(4," 123459");
+console.log(padded1)
+
+let pad3= "Other"
+console.log(pad3.padEnd(11," Bite")); // output= Other Bite
