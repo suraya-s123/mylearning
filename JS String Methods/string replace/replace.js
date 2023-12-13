2bf47b873ae519c877939b990369f24975b8f91c
suraya-s123 <sarminsuraya@gmail.com>
2023-12-13 09:58:18 +0600

    String replace property add

---
+++ JS String Methods/string replace/replace.js
@@ -0,0 +1,12 @@
+let text = "Please Visit Website My Website";
+let newText= text.replace("Website","House");
+console.log(newText);
+
+
+// replaceAll
+
+let txt= "I love dogs. Dogs are very easy to love. Dogs are very popular ";
+ txt= txt.replaceAll("dogs","animal");
+ txt= txt.replaceAll("Dogs","animal");
+
+console.log(txt);
